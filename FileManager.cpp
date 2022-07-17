b4f4b03ffc2fe57ea477a1b8377dc37fe872264b
Vadim Artemov <93925370+ElJohnSoap@users.noreply.github.com>
2022-07-17 06:50:30 +0300

    Add project files.

---
+++ FileManager.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+#include <io.h>
+#include <stdio.h>
+#include <string.h>
+#include "Manager.h"
+using namespace std;
+
+
+int main()
+{
+    
+}
+
---
+++ Manager.h
@@ -0,0 +1,9 @@
+#pragma once
+class Manager
+
+{
+private:
+	string rootDirectory = ;
+
+};
+
